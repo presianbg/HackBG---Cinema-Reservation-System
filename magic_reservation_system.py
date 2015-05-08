3e016f568ade7bae67b32cff06035d03a22bd2bd
presianbg <presianbg@gmail.com>
2015-05-08 13:24:06 +0300

    Minor Changes to CLI

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -5,6 +5,10 @@ from tabulate import tabulate
 
 class CinemaReservation:
 
+    GET_MOMIVES_BY_RATING = '''
+        SELECT * FROM Movies ORDER BY rating DESC
+    '''
+
     @staticmethod
     def create_help():
         help = ["Here is the list of commands:",
@@ -16,21 +20,25 @@ class CinemaReservation:
                 "exit                                    : Exits reservation system"]
         return "\n".join(help)
 
-    @classmethod
-    def parse_command(cls, command):
+    @staticmethod
+    def parse_command(command):
         return tuple(command.split(" "))
+
+    @staticmethod
+    def is_command(command_tuple, command_string):
+        return command_tuple[0] == command_string
 
     @classmethod
     def show_movies(cls, connection):
         pptable = []
         headers = ["Movie Name", "Movie Rating"]
         cursor = connection.cursor()
-        cursor.execute('''SELECT * FROM Movies ORDER BY rating DESC''')
+        cursor.execute(cls.GET_MOMIVES_BY_RATING)
         movies_by_rating = cursor.fetchall()
         for row in movies_by_rating:
             pptable.append(row[1], row[2])
         return tabulate(pptable, headers, tablefmt="fancy_grid")
 
     @classmethod
-    def is_command(cls, command_tuple, command_string):
-        return command_tuple[0] == command_string
+    def show_movie_projections(cls, connection, movie_id, date='*'):
+        pass
